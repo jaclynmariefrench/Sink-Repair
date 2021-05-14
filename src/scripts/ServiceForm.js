90024ba2042ebb2fa9c94d23fa9a2c459d19a33a
Jaclyn French <jaclynmariefrench@gmail.com>
2021-05-14 10:33:44 -0500

    finished styling

--- src/scripts/ServiceForm.js
+++ src/scripts/ServiceForm.js
@@ -3,19 +3,23 @@
 export const ServiceForm = () => {
     let html = `
         <div class="field">
-            <label class="label" for="serviceDescription">Description</label>
+            <label class="label__description" for="serviceDescription">Description</label>
+            <div></div>
             <input type="text" name="serviceDescription" class="input" />
         </div>
         <div class="field">
-            <label class="label" for="serviceAddress">Address</label>
+            <label class="label__address" for="serviceAddress">Address</label>
+            <div></div>
             <input type="text" name="serviceAddress" class="input" />
         </div>
         <div class="field">
-            <label class="label" for="serviceBudget">Budget</label>
+            <label class="label__budget" for="serviceBudget">Budget</label>
+            <div></div>
             <input type="number" name="serviceBudget" class="input" />
         </div>
         <div class="field">
-            <label class="label" for="serviceDate">Date needed</label>
+            <label class="label__serviceDate" for="serviceDate">Date needed</label>
+            <div></div>
             <input type="date" name="serviceDate" class="input" />
         </div>
 
